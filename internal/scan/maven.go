16cabf720a37771cf533182aa67e320dd4e210b8
mcoops <cooper.d.mark@gmail.com>
2021-05-01 02:50:10 +1000

    maven: grab package name
    Signed-off-by: mcoops <cooper.d.mark@gmail.com>

--- internal/scan/maven.go
+++ internal/scan/maven.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
@@ -19,11 +20,17 @@ func gatherMvn(mvn string) (string, string, error) {
 	}
 	mvnStr = mvnStr[:lastColon]
 
-	verIdx := strings.LastIndex(mvnStr, ":")
-	if verIdx == -1 || len(mvnStr) < (verIdx+1) {
+	mvn = strings.Replace(mvn, ":compile", "", 1)
+	mvn = strings.Replace(mvn, ":provided", "", 1)
+
+	verIdx := strings.LastIndex(mvn, ":")
+	if verIdx == -1 || len(mvn) < (verIdx+1) {
 		return "", "", fmt.Errorf("Invalid maving parsing, looking for version ':'")
 	}
-	ver := mvnStr[verIdx+1:]
+	ver := mvn[verIdx+1:]
+
+	ver = strings.TrimSuffix(ver, "\" ; ") // TOOD watch whitespace
+	ver = strings.TrimSuffix(ver, " (optional) ")
 
 	if mvnStr[0] == '"' {
 		mvnStr = mvnStr[1:]
@@ -34,6 +41,7 @@ func gatherMvn(mvn string) (string, string, error) {
 }
 
 func GetMvnDeps(path string) ([][2]string, error) {
+	r := regexp.MustCompile("\"(.*?)\"")
 	var gathered [][2]string // array of [name, ver]string
 
 	seen := make(map[string]struct{})
@@ -72,11 +80,7 @@ func GetMvnDeps(path string) ([][2]string, error) {
 		if sepIdx != -1 {
 			// skip import and test
 			// avoid errors downloading deps, not much we can do here
-			// if strings.Contains(s, ":test") || strings.Contains(s, ":import") || strings.Contains(s, "ERROR") {
-			// 	continue
-			// }
-
-			if strings.Contains(s, "ERROR") {
+			if strings.Contains(s, ":test") || strings.Contains(s, ":import") || strings.Contains(s, "ERROR") {
 				continue
 			}
 
@@ -97,6 +101,38 @@ func GetMvnDeps(path string) ([][2]string, error) {
 				seen[repo+version] = struct{}{}
 			}
 
+		} else {
+			if strings.Contains(s, "digraph") {
+				if strings.Contains(s, ":pom:") {
+					continue
+				}
+
+				if strings.Contains(s, ":bundle") {
+					fmt.Println("here")
+				}
+				s = strings.Replace(s, ":bundle", "", 1)
+				var part string
+				// record the package name
+				res := r.FindStringSubmatch(s)
+				if len(res) > 1 {
+					part = res[1]
+				} else {
+					continue
+				}
+				repo, version, err := gatherMvn(part)
+
+				// only if no error append
+				if err == nil {
+
+					// lookup first if we have an entry
+					if _, ok := seen[repo+version]; ok {
+						continue
+					}
+
+					gathered = append(gathered, [2]string{repo, version})
+					seen[repo+version] = struct{}{}
+				}
+			}
 		}
 	}
 	return gathered, nil
